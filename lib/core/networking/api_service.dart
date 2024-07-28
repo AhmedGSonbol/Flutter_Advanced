62027a9e7af3ae597443aa514ad315eafc6306e6
Ahmed Gehad Sonbol <ahmedsonbl9@gmail.com>
2024-07-28 09:08:21 +0300

    api services

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,24 @@
+
+import 'package:dio/dio.dart';
+import 'package:flutter_advanced/core/networking/api_constants.dart';
+import 'package:flutter_advanced/features/login/data/models/login_request_body.dart';
+import 'package:flutter_advanced/features/login/data/models/login_response.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseApiUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponse> login(
+      @Body() LoginRequestBody loginRequestBody,
+      );
+
+  //
+  // @POST(ApiConstants.signup)
+  // Future<SignupResponse> signup(
+  //     @Body() SignupRequestBody signupRequestBody,
+  //     );
+}
--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorsManager
 {
+
   static const Color mainColor = Color(0xFF247CFF);
   static const Color gray = Color(0xFF757575);
   static const Color lightgray = Color(0xFFC2C2C2);
